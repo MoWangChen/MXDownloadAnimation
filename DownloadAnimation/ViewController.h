050001d9130a8f69945d5b60121c5105923fbd6f
xiepengxiang <xiepengxiang@365ime.com>
2016-08-12 16:28:50 +0800

    init project

---
+++ DownloadAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadAnimation
+//
+//  Created by 谢鹏翔 on 16/8/12.
+//  Copyright © 2016年 ime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DownloadAnimation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DownloadAnimation
+//
+//  Created by 谢鹏翔 on 16/8/12.
+//  Copyright © 2016年 ime. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
